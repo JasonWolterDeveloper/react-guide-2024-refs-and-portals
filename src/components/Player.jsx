1add8761d856011eef0bbb78e832b2f981ffcf5d
JasonWolterDeveloper <143658631+JasonWolterDeveloper@users.noreply.github.com>
2024-04-24 13:37:30 -0500

    Initial Saving Player Name implementation

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayer, setEnteredPlayer] = useState('');
+  const [playerName, setPlayerName] = useState('')
+
+  const enteredNameChangeHandler = (event) => {
+    setEnteredPlayer(event.target.value)
+  }
+
+  const setNameHandler = () => {
+    setPlayerName(enteredPlayer)
+  }
+
+  let playerNameComponent = <h2>Welcome unknown entity</h2>
+
+  if (playerName !== '') {
+    playerNameComponent = <h2>Welcome {playerName}</h2>
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {playerNameComponent}
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input onChange={enteredNameChangeHandler} value={enteredPlayer} type="text" />
+        <button onClick={setNameHandler}>Set Name</button>
       </p>
     </section>
   );
